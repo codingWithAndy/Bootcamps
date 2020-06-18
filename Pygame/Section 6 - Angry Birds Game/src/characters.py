a122568e80e728a62374ad0fd66c510232135d20
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2020-06-18 16:31:22 +0100

    Angry birds finished. Racing game start.

--- Pygame/Section 6 - Angry Birds Game/src/characters.py
+++ Pygame/Section 6 - Angry Birds Game/src/characters.py
@@ -1,49 +1,40 @@
-from pymunk as pm 
-
+import pymunk as pm
 from pymunk import Vec2d
 
-class Bird():
 
+class Bird():
     def __init__(self, distance, angle, x, y, space):
         self.life = 20
         mass = 5
         radius = 12
-        inertia = pm.moment_for_circle(mass,0,radius,(0,0))
-        body = pm.Body(mass,inertia)
-
-        body.position = x,y
+        inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
+        body = pm.Body(mass, inertia)
+        body.position = x, y
         power = distance * 53
-        impulse = power * Vec2d(1,0)
-
+        impulse = power * Vec2d(1, 0)
         angle = -angle
         body.apply_impulse_at_local_point(impulse.rotated(angle))
-
-        shape = pm.Circle(body,radius,(0,0))
-
+        shape = pm.Circle(body, radius, (0, 0))
         shape.elasticity = 0.95
-        self.friction = 1
-
+        shape.friction = 1
         shape.collision_type = 0
-
-        space.add(body,shape)
+        space.add(body, shape)
         self.body = body
         self.shape = shape
 
-class Pig():
 
+class Pig():
     def __init__(self, x, y, space):
         self.life = 20
         mass = 5
         radius = 14
-
-        inertia = pm.moment_for_circle(mass,0,radius,(0,0))
-        body = pm.Body(mass,inertia)
-        shape = pm.Circle(body,radius,(0,0))
-
+        inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
+        body = pm.Body(mass, inertia)
+        body.position = x, y
+        shape = pm.Circle(body, radius, (0, 0))
         shape.elasticity = 0.95
-        self.friction = 1
-        
+        shape.friction = 1
         shape.collision_type = 1
-
+        space.add(body, shape)
         self.body = body
         self.shape = shape
